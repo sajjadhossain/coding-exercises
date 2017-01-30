af7aa3edf3be77c8389e2aadb1c94da964305b97
sajjadhossain <sajjad@withpulp.com>
2017-01-30 06:39:15 -0500

    commented out WIP

--- javascript/test/specs/compoundWords.js
+++ javascript/test/specs/compoundWords.js
@@ -7,13 +7,13 @@ var wordsToTest = {
   result: [false, true, false, true, true]
 }
 
-describe('is compound word', function() {
-  var isCompoundWord = require('../../compoundWords').isCompoundWord;
-
-  _.forEach(wordsToTest.input, function (word) {
-    it(/*'isCompoundWord(' + words.toString() + ')' + ' => ' + object.result*/'', function() {
-      isCompoundWord(word, wordsToTest.input)
-      // chai.assert(doObjectsEqual(object[1], object[2]) === object.result, 'Expected ' + doObjectsEqual(object[1], object[2]) + ' to equal ' + object.result);
-    });
-  });
-});
+// describe('is compound word', function() {
+//   var isCompoundWord = require('../../compoundWords').isCompoundWord;
+//
+//   _.forEach(wordsToTest.input, function (word) {
+//     it(/*'isCompoundWord(' + words.toString() + ')' + ' => ' + object.result*/'', function() {
+//       isCompoundWord(word, wordsToTest.input)
+//       // chai.assert(doObjectsEqual(object[1], object[2]) === object.result, 'Expected ' + doObjectsEqual(object[1], object[2]) + ' to equal ' + object.result);
+//     });
+//   });
+// });
