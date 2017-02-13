471c7c6633287226887ae7d6c2a3dff53471c933
sajjadhossain <sajjad@withpulp.com>
2017-02-13 17:46:32 -0500

    updated readme, starting get neighbors

---
+++ javascript/getNeighbors.js
@@ -0,0 +1,5 @@
+// Objective: Write a function that returns 4 neighboring coordinates for (0,0)
+
+module.exports = function(coordinates) {
+  return coordinates;
+}
---
+++ javascript/test/specs/getNeighbors.js
@@ -0,0 +1,21 @@
+var chai = require('chai');
+var coordinates = '0,0';
+var expectedResult = {
+  1: '1,1',
+  2: '1,0',
+  3: '1,-1',
+  4: '0,-1',
+  5: '-1,-1',
+  6: '-1,0',
+  7: '-1,1',
+  8: '1,0'
+};
+
+describe('is compound word', function() {
+  var getNeighbors = require('../../getNeighbors');
+
+  it('getNeighbors(' + coordinates + ')' + ' => ' + expectedResult, function() {
+    console.log(getNeighbors(coordinates))
+    // chai.assert(getNeighbors(coordinates) === expectedResult, 'Expected ' + getNeighbors(coordinates) + ' to equal ' + expectedResult);
+  });
+});
